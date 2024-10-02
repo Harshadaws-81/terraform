b0531b56eed78a49a2a8f313d7a42798b7d84f6c
harshavardhan <kadiamharsha27@gmail.com>
2024-10-02 20:24:33 +0530

    Terraform

--- expense/r53.tf
+++ expense/r53.tf
@@ -2,7 +2,7 @@ resource "aws_route53_record" "expense" {
   count = length(var.instance_names)
   zone_id = var.zone_id
   # mysql.harshadaws81s.online
-  name    = "${var.instance_names[count.index]}.${var.domain_name}"
+  name    = "${var.instance_names[count.index]}.${var.domain_name}" 
   type    = "A"
   ttl     = 1
   records = [aws_instance.expense[count.index].private_ip]
