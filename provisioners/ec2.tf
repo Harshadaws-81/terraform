bc1fcd3e97781ae26520674fac97d9f3e1c1018c
harshavardhan <kadiamharsha27@gmail.com>
2024-10-08 12:35:35 +0530

    Terraform - provisioners

---
+++ provisioners/ec2.tf
@@ -0,0 +1,64 @@
+resource "aws_security_group" "allow_ssh_terraform" {
+  name        = "allow_sshhh" # allow_ssh is already there in my account
+  description = "Allow port number 22 for SSH Access"
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  ingress {
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  ingress {
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+}
+
+resource "aws_instance" "terraform" {
+  ami                    = "ami-09c813fb71547fc4f"
+  instance_type          = "t3.micro"
+  vpc_security_group_ids = [aws_security_group.allow_ssh_terraform.id]
+  tags = {
+    Name = "Terraform"
+  }
+  # In this case my Laptop is Local
+  provisioner "local-exec" {
+    command = "echo ${self.private_ip} > public_ip.txt"
+  }
+  # Connection should be established for Remote execution
+  connection {
+    type     = "ssh"
+    user     = "ec2-user"
+    password = "DevOps321"
+    host     = self.public_ip
+  }
+
+  # provisioners will execute at the time of creation
+  provisioner "remote-exec" {
+    inline = [
+      "sudo dnf install ansible -y",
+      "sudo dnf install nginx -y",
+      "sudo systemctl start nginx",
+    ]
+  }
+
+  provisioner "remote-exec" {
+    when = destroy
+    inline = [
+      "sudo systemctl stop nginx"
+    ]
+  }
+}
